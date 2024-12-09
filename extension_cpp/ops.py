410d84463bd64d7af00bb49f49507147ab9b08e2
Filippo Biondi <filippo22@ru.is>
2024-12-09 16:07:05 +0100

    fix

--- extension_cpp/ops.py
+++ extension_cpp/ops.py
@@ -1,7 +1,7 @@
 import torch
 from torch import Tensor
 
-__all__ = ["mymuladd", "myadd_out"]
+__all__ = ["mymuladd"]
 
 
 def mymuladd(a: Tensor, b: Tensor, c: float) -> Tensor:
@@ -21,43 +21,3 @@ def _(a, b, c):
     torch._check(a.device == b.device)
     return torch.empty_like(a)
 
-
-def _backward(ctx, grad):
-    a, b = ctx.saved_tensors
-    grad_a, grad_b = None, None
-    if ctx.needs_input_grad[0]:
-        grad_a = torch.ops.extension_cpp.mymul.default(grad, b)
-    if ctx.needs_input_grad[1]:
-        grad_b = torch.ops.extension_cpp.mymul.default(grad, a)
-    return grad_a, grad_b, None
-
-
-def _setup_context(ctx, inputs, output):
-    a, b, c = inputs
-    saved_a, saved_b = None, None
-    if ctx.needs_input_grad[0]:
-        saved_b = b
-    if ctx.needs_input_grad[1]:
-        saved_a = a
-    ctx.save_for_backward(saved_a, saved_b)
-
-
-# This adds training support for the operator. You must provide us
-# the backward formula for the operator and a `setup_context` function
-# to save values to be used in the backward.
-torch.library.register_autograd(
-    "extension_cpp::mymuladd", _backward, setup_context=_setup_context)
-
-
-@torch.library.register_fake("extension_cpp::mymul")
-def _(a, b):
-    torch._check(a.shape == b.shape)
-    torch._check(a.dtype == torch.float)
-    torch._check(b.dtype == torch.float)
-    torch._check(a.device == b.device)
-    return torch.empty_like(a)
-
-
-def myadd_out(a: Tensor, b: Tensor, out: Tensor) -> None:
-    """Writes a + b into out"""
-    torch.ops.extension_cpp.myadd_out.default(a, b, out)
